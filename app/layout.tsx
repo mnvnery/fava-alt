edd3f0782726f9ed1df535d5a0ef96d8b0f68f70
Maria Nery <mnvnery@gmail.com>
2025-02-11 10:27:36 +0000

    outputs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
